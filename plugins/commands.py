32af3fb566237112a387302405ebdc7bf671337c
SaikatFTW <84172414+saikatwtf@users.noreply.github.com>
2022-11-26 20:17:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -252,7 +252,6 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/c/GreyMattersBot') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
