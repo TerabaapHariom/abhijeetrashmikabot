dbb8d1223bb9edbf7abc606f34146c99f1a68c8e
SaikatFTW <84172414+saikatwtf@users.noreply.github.com>
2022-11-26 18:57:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -377,7 +377,7 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
-async async def get_shortlink(link):
+async def get_shortlink(link):
     https = link.split(":")[0]
     if "http" == https:
         https = "https"
