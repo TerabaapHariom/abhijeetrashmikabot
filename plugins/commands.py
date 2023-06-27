1d9f787e8e75d825c845185637c3c1d2d566e123
TerabaapHariom <100013636+TerabaapHariom@users.noreply.github.com>
2023-06-27 13:03:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -43,7 +43,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('нσω тσ υѕє', url=f'http://telegram.dog/Links2Play/17')
+            InlineKeyboardButton('нσω тσ υѕє', url=f'http://telegram.dog/SouthTamilall2/24')
         ], [
             InlineKeyboardButton('✘ Latest Updates', url='https://telegram.dog/SouthTamilall1'),
             InlineKeyboardButton('Bot Updates ✘', url='https://telegram.dog/SouthTamilallbots')
