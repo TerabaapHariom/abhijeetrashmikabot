34d4ad022aa43c32d7153202f5b37fe5bc8724e6
Daneil-k <158211667+Daneil-k@users.noreply.github.com>
2024-04-14 21:05:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Tnlink")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Clustor0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
